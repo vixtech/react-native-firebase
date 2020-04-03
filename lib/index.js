vixtech/react-native-firebase

commit 5b1717ff6112217ce34b5a38c022bd78da241a14

bug(analytics): logEvent params can be objects (#3219)
* bug(analytics): logEvent params can be objects

* test(analytics): removed unnecessary test

lib/index.js
@@ -23,6 +23,7 @@ import {
   isOneOf,
   isString,
   isUndefined,
+  isArray,
 } from '@react-native-firebase/app/lib/common';
 import { validateStruct, validateCompound } from '@react-native-firebase/app/lib/common/struct';
 
@@ -90,12 +91,12 @@ class FirebaseAnalyticsModule extends FirebaseModule {
     }
 
     const entries = Object.entries(params);
+
     for (let i = 0; i < entries.length; i++) {
       const [key, value] = entries[i];
-      if (!isString(value) && !isNumber(value) && !isBoolean(value)) {
-        throw new Error(
-          `firebase.analytics().logEvent(_, *) 'params' value for parameter '${key}' is invalid, expected a string or number value.`,
-        );
+
+      if (isObject(value) || isArray(value)) {
+        params[key] = value.toString();
       }
     }
 

e2e/analytics.e2e.js
@@ -111,21 +111,6 @@ describe('analytics()', () => {
       }
     });
 
-    it('errors if params contains invalid types', () => {
-      try {
-        firebase.analytics().logEvent('invertase', {
-          foo: 'bar',
-          bar: {
-            baz: 123,
-          },
-        });
-        return Promise.reject(new Error('Did not throw.'));
-      } catch (e) {
-        e.message.should.containEql("'params' value for parameter 'bar' is invalid");
-        return Promise.resolve();
-      }
-    });
-
     it('log an event without parameters', async () => {
       await firebase.analytics().logEvent('invertase_event');
     });

commit 4bc9517fb13248b09c7a2bf42c344f657e7b535c

fix(analytics): logEvent params can be objects, further fix for… (#3351)
* fix(analytics): pass logEvent params as-is broken in #3219
* fix(types,analytics): logEvent types for #3219
* refactor(types,analytics): simplify logEvent type

lib/index.js
@@ -90,16 +90,6 @@ class FirebaseAnalyticsModule extends FirebaseModule {
       );
     }
 
-    const entries = Object.entries(params);
-
-    for (let i = 0; i < entries.length; i++) {
-      const [key, value] = entries[i];
-
-      if (isObject(value) || isArray(value)) {
-        params[key] = value.toString();
-      }
-    }
-
     return this.native.logEvent(name, params);
   }
 

lib/index.js.flow
@@ -623,7 +623,7 @@ export interface Module extends ReactNativeFirebaseModule {
    * @param name Event name must not conflict with any Reserved Events. 100 characters is the maximum length for param key names.
    * @param params Parameters to be sent and displayed with the event.
    */
-  logEvent(name: string, params?: { [key: string]: string  | number | boolean } | {}): Promise<void>;
+  logEvent(name: string, params?: { [key: string]: any }): Promise<void>;
 
   /**
    * If true, allows the device to collect analytical data and send it to Firebase.